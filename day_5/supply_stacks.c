ced40a7eecf0f8db2d72b121180a11a7466637c9
The_philosopher <email.shinudonney@gmail.com>
2022-12-06 14:18:35 +0530

    Finish day 5 exercise part 2

--- day_5/supply_stacks.c
+++ day_5/supply_stacks.c
@@ -10,79 +10,88 @@
 char pop_element(char str[]);
 void add_element(char str[], char element);
 int get_line(char line[], int limit);
+void cut_and_join(char src[], char dest[], int position);
 
 int main() {
-    int INSTRUCTION_FLAG = 0, length, n, src, dest;
-    char line[LINE_LIMIT] = {'\0'}, *i, crate;
-    char stacks[9][STACK_SIZE] = {"FCJPHTW", "GRVFZJBH", "HPTR", "ZSNPHT", "NVFZHJCD",
-        "PMGFWDZ", "MVZWSJDP", "NDS",  "DZSFM"};
-    while ((length = get_line(line, LINE_LIMIT)) > 0) {
-        if (INSTRUCTION_FLAG) {
-            i = strtok(line, " ");
-            i = strtok(NULL, " ");
-            n = atoi(i);
-            i = strtok(NULL, " ");
-            i = strtok(NULL, " ");
-            src = atoi(i) - 1;
-            i = strtok(NULL, " ");
-            i = strtok(NULL, " ");
-            dest = atoi(i) - 1;
-            for (int j = 0; j < n; j++) {
-                crate = pop_element(stacks[src]);
-                if (!crate) {
-                    printf("Error. Stack empty!\n");
-                    return 1;
-                }
-                add_element(stacks[dest], crate);
-            }
-        }
-        if (length == 1) {
-            INSTRUCTION_FLAG = 1;
-        }
+  int INSTRUCTION_FLAG = 0, length, n, src, dest;
+  char line[LINE_LIMIT] = {'\0'}, *i;
+  char stacks[9][STACK_SIZE] = {"FCJPHTW",  "GRVFZJBH", "HPTR",
+                                "ZSNPHT",   "NVFZHJCD", "PMGFWDZ",
+                                "MVZWSJDP", "NDS",      "DZSFM"};
+  while ((length = get_line(line, LINE_LIMIT)) > 0) {
+    if (INSTRUCTION_FLAG) {
+      i = strtok(line, " ");
+      i = strtok(NULL, " ");
+      n = atoi(i);
+      i = strtok(NULL, " ");
+      i = strtok(NULL, " ");
+      src = atoi(i) - 1;
+      i = strtok(NULL, " ");
+      i = strtok(NULL, " ");
+      dest = atoi(i) - 1;
+      cut_and_join(stacks[src], stacks[dest], n);
     }
-    int last_element_position;
-    for(int i = 0; i<9; i++) {
-        last_element_position = strlen(stacks[i]) - 1;
-        printf("%c", stacks[i][last_element_position]);
+    if (length == 1) {
+      INSTRUCTION_FLAG = 1;
     }
-    printf("\n");
-    return 0;
+  }
+  int last_element_position;
+  for (int i = 0; i < 9; i++) {
+    last_element_position = strlen(stacks[i]) - 1;
+    printf("%c", stacks[i][last_element_position]);
+  }
+  printf("\n");
+  return 0;
 }
 
 int get_line(char line[], int limit) {
-    int i, c;
-    for (i = 0; i < limit; i++) {
-        c = getchar();
-        if (c == EOF) {
-            break;
-        }
-        line[i] = c;
-        if (c == '\n') {
-            ++i;
-            break;
-        }
+  int i, c;
+  for (i = 0; i < limit; i++) {
+    c = getchar();
+    if (c == EOF) {
+      break;
     }
-    line[i] = '\0';
-    return i;
+    line[i] = c;
+    if (c == '\n') {
+      ++i;
+      break;
+    }
+  }
+  line[i] = '\0';
+  return i;
 }
 
 void add_element(char str[], char element) {
-    int length = strlen(str);
-    if (length + 1 <= STACK_SIZE) {
-        str[length] = element;
-    }
-    else {
-        printf("Failed to add element. Stack full.\n");
-    }
-    return;
+  int length = strlen(str);
+  if (length + 1 <= STACK_SIZE) {
+    str[length] = element;
+  } else {
+    printf("Failed to add element. Stack full.\n");
+  }
+  return;
 }
 
 char pop_element(char str[]) {
-    char last_element = '\0';
-    int length = strlen(str);
-    if (length > 0) {
-        last_element = str[length - 1];
-        str[length - 1] = '\0';
+  char last_element = '\0';
+  int length = strlen(str);
+  if (length > 0) {
+    last_element = str[length - 1];
+    str[length - 1] = '\0';
+  }
+  return last_element;
+}
+
+// position is negetive indexed, ie starts from behind
+void cut_and_join(char src[], char dest[], int position) {
+  int src_length = strlen(src);
+  int dest_length = strlen(dest);
+  if (src_length >= position) {
+    for (int i = 0; i < position; i++) {
+      dest[dest_length + i] = src[src_length - position + i];
     }
-    return last_element;
+    for (int i = src_length - position; i < STACK_SIZE; i++)
+      src[i] = '\0';
+  } else {
+    printf("Error. Operation not possible.\n");
+  }
 }
